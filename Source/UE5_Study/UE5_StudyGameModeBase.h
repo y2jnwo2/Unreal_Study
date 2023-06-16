7232a9aef3fe9a99f52708cc0000cdd53f546d23
[o0ojenny] <[o0o_jenny@naver.com]>
2023-06-16 11:29:11 +0900

    do basic

---
+++ Source/UE5_Study/UE5_StudyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5_StudyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_STUDY_API AUE5_StudyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
